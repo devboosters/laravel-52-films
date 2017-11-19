58253250248c29e9ccf722cdf325de75f8e4660a
Naeem Choudhary <rordeveloper28@gmail.com>
2017-11-19 13:38:00 +0500

    Initial Commit with required packages and other dependencies and some basic features

---
+++ app/Models/Access/Comment/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Access\Comment;
+
+use Illuminate\Database\Eloquent\Model;
+use Actuallymab\LaravelComment\Comment as ParentComment;
+
+class Comment extends ParentComment
+{
+
+}
+
+
+
+
+
+
---
+++ resources/assets/js/frontend/app.js
@@ -0,0 +1,5 @@
+/*Frontend App scripting */
+$(function(){
+
+    console.log("in the Landing Page of the Frontend App");
+});
